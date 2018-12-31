d11843cb40cacb0fc13986cd379a071995d3b9e6
Tony Bao <kafka0912@gmail.com>
2018-12-30 23:12:57 -0500

    array

--- 1230/【260】Interleave Positive And Negative Elements.java
+++ 1230/【260】Interleave Positive And Negative Elements.java
@@ -1,4 +1,20 @@
 /*
+Description
+Given an array with both positive and negative numbers in random order. Shuffle the array so that positive and negative numbers are put in position with even and odd indices, respectively.
+
+If there are more positive/negative numbers, put them at the end of the array. The ordering of positive/negative numbers does not matter.
+
+Assumptions:
+
+The given array is not null.
+There is no 0 in the array.
+Examples:
+
+{1, 2, 3, 4, 5, -1, -1, -1} --> {1, -1, 2, -1, 3, -1, 4, 5}  (The ordering of positive/negative numbers do not matter)
+
+*/
+
+/*
   to be reconsidered with more efficient solution.
   
   in this in-place solution, in first traversal, move all negative elements to the right part.
