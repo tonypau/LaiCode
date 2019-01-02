1b020ef9b2860980d9b3951b5001063ad8cef8ba
Tony Bao <kafka0912@gmail.com>
2019-01-01 23:05:28 -0500

    array

--- 0101/【283】String Distance.java
+++ 0101/【283】String Distance.java
@@ -12,42 +12,25 @@ Examples:
 array =  {"this", "is", "a", "is", "fox", "happy"}, the distance of "fox" and "is" is 1.
 */
 
-// O(n)
 public class Solution {
-  public int distance(String[] array, String source, String target) {
-    // hashmap record apperance indices of unique string in array
-    Map<String, List<Integer>> map = new HashMap<>();
-    for (int i = 0; i < array.length; i++) {
-      List<Integer> list = map.get(array[i]);
-      if (list == null) {
-        List<Integer> l = new ArrayList<>();
-        l.add(i);
-        map.put(array[i], l);
-      } else {
-        list.add(i);
-        map.put(array[i], list);
-      }
-    }
-    // find the least diff in two lists which are sorted already
-    List<Integer> l1 = map.get(source);
-    List<Integer> l2 = map.get(target);
-    if (l1 != null && l2 != null) {
-      int i = l1.size() - 1;
-      int j = l2.size() - 1;
-      int diff = Integer.MAX_VALUE;
-      while (i >= 0 && j >= 0) {
-        int val_s = l1.get(i);
-        int val_t = l2.get(j);
-        if (val_s < val_t) {
-          j--;
-          diff = Math.min(diff, val_t - val_s);
-        } else {
-          i--;
-          diff = Math.min(diff, val_s - val_t);
+  public int shortestDistance(String[] words, String word1, String word2) {
+    int m = -1;
+    int n = -1;
+    int min = Integer.MAX_VALUE;
+    for (int i = 0; i < words.length; i++) {
+      String s = words[i];
+      if (s.equals(word1)) {
+        m = i;
+        if (n != -1) {
+          min = Math.min(min, m - n);
+        }
+      } else if (s.equals(word2)) {
+        n = i;
+        if (m != -1) {
+          min = Math.min(min, n - m);
         }
       }
-      return diff;
     }
-    return -1;
+    return min;
   }
 }
