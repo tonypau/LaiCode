8db27f80b08dc57a606f4ded43ecf5e914a79090
Tony Bao <kafka0912@gmail.com>
2019-01-04 14:30:01 -0500

    linked list

---
+++ 0104/【462】Odd Even Linked List.java
@@ -0,0 +1,46 @@
+/*
+Given a singly linked list, group all odd nodes together followed by the even nodes. Please note here we are talking about the node number and not the value in the nodes.
+
+You should try to do it in place. The program should run in O(1) space complexity and O(nodes) time complexity.
+
+Example:
+Given 1->2->3->4->5->NULL,
+return 1->3->5->2->4->NULL.
+
+Note:
+The relative order inside both the even and odd groups should remain as it was in the input. 
+The first node is considered odd, the second node even and so on ...
+*/
+
+/**
+ * class ListNode {
+ *   public int value;
+ *   public ListNode next;
+ *   public ListNode(int value) {
+ *     this.value = value;
+ *     next = null;
+ *   }
+ * }
+ */
+public class Solution {
+  public ListNode oddEvenList(ListNode head) {
+    if (head == null) {
+      return head;
+    }    
+    ListNode p1 = head;
+    ListNode newHead = head.next;
+    ListNode p2 = newHead;
+    while (p1 != null && p2 != null) {
+      ListNode t = p2.next;
+      if (t == null) break;
+      
+      p1.next = t;
+      p1 = p1.next;
+      
+      p2.next = p1.next;
+      p2 = p2.next;
+    }
+    p1.next = newHead;
+    return head;
+  }
+}
