473786663d9b6c19ad4603d5fc2d0dd097187c72
Tony Bao <kafka0912@gmail.com>
2019-01-04 16:08:17 -0500

    math

--- 0104/【311】Prime Factors.java
+++ 0104/【311】Prime Factors.java
@@ -1,3 +1,15 @@
+/*
+Each positive integer larger than 1 is the production of several prime numbers. Return the list of prime factors in ascending order.
+
+Assumptions:
+
+The given number is >= 2.
+Examples:
+
+12 = 2 * 2 * 3, return [2, 2, 3]
+5 = 5, return [5]
+*/
+
 public class Solution {
   public List<Integer> factors(int target) {
     List<Integer> res = new ArrayList<>();
