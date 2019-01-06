ebe14e71b2461c1098b36d9b95392084ed5b535c
Tony Bao <kafka0912@gmail.com>
2019-01-06 15:27:30 -0500

    bst

--- 0106/【347】Second Largest In Binary Search Tree.java
+++ 0106/【347】Second Largest In Binary Search Tree.java
@@ -40,27 +40,27 @@ public class Solution {
       
       // when cur has no right child but has left child
       // we find the largest one in its left child
-			if (cur.left != null && cur.right == null) {
-				return largest(cur.left);
-			}
+      if (cur.left != null && cur.right == null) {
+        return largest(cur.left);
+      }
       
       // when cur has right child but its right child has neight left or right child, 
       // then cur is the second largest we need
-			if (cur.right != null && cur.right.left == null && cur.right.right == null) {
-				return cur.key;
-			}
-			cur = cur.right;
-		}
+      if (cur.right != null && cur.right.left == null && cur.right.right == null) {
+        return cur.key;
+      }
+      cur = cur.right;
+    }
 		
     return Integer.MIN_VALUE;
-	}
+  }
 	
-	private int largest(TreeNode root) {
-		TreeNode cur = root;
-		while (root.right != null) {
-			root = root.right;
-		}
-		return root.key;
-	}
+  private int largest(TreeNode root) {
+    TreeNode cur = root;
+    while (root.right != null) {
+      root = root.right;
+    }
+    return root.key;
+  }
 }
 
