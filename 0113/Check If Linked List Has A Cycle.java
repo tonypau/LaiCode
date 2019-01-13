0b09869e73da336f2db4b2d656e9597f170eefea
Tony Bao <kafka0912@gmail.com>
2019-01-13 17:00:28 -0500

    two pointers, linkedlist

---
+++ 0113/Check If Linked List Has A Cycle.java
@@ -0,0 +1,24 @@
+/**
+ * class ListNode {
+ *   public int value;
+ *   public ListNode next;
+ *   public ListNode(int value) {
+ *     this.value = value;
+ *     next = null;
+ *   }
+ * }
+ */
+public class Solution {
+  public boolean hasCycle(ListNode head) {
+    if (head == null || head.next == null) return false;
+    ListNode slow = head;
+    ListNode fast = head.next;
+    while (fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+      if (slow == fast) return true;
+    }
+    return false;
+  }
+}
+
