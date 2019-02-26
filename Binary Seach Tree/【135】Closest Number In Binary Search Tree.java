8f25dc0dd572fecba541433e52e537f5e3102b51
Tony Bao <kafka0912@gmail.com>
2019-02-26 15:20:06 -0500

    Find Value in BST

--- Binary Seach Tree/【135】Closest Number In Binary Search Tree.java
+++ Binary Seach Tree/【135】Closest Number In Binary Search Tree.java
@@ -3,7 +3,7 @@
 // for each step: compare previous closest with cur root, which is closer, then enter into next level
 
 // TC: O(height)
-// SC: O(height)
+// SC: O(1)
 public class Solution {
   public int closest(TreeNode root, int target) {
     int closest = root.key;
