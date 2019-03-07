1529ea262f4a703fb9c54dc8a47c3ee9b35df1f2
Tony Bao <kafka0912@gmail.com>
2019-03-06 23:46:12 -0500

    dp

--- March/06/【607】Longest Cycle.java
+++ March/06/【607】Longest Cycle.java
@@ -3,28 +3,33 @@
 
 public class Solution {
   public int longest(int[] nums) {
-    if (nums == null || nums.length == 0) return 0;
     int max = 0;
     int[] dp = new int[nums.length];
     for (int i = 0; i < nums.length; i++) {
-      dp[i] = helper(dp, nums, i);
+      helper(dp, nums, i);
       max = Math.max(max, dp[i]);
     }
     return max;
   }
 
-  private int helper(int[] dp, int[] nums, int i) {
+  private void helper(int[] dp, int[] nums, int i) {
     int num = nums[i];
     int tmp = nums[i];
     int count = 1;
+    List<Integer> list = new ArrayList<>();
+    list.add(i);
     while (num != nums[tmp]) {
       count++;
       if (dp[tmp] != 0) {
-        return dp[tmp];
+        dp[i] = dp[tmp];
+        return;
       }
+      list.add(tmp);
       tmp = nums[tmp];
     }
-    return count;
+    for (int index : list) {
+      dp[index] = count;
+    }
   }
 }
 
