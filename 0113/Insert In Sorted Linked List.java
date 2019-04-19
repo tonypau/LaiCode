f5ef6245a2e249572d3802f38a66ea36a7729d8a
Tony Bao <kafka0912@gmail.com>
2019-04-19 19:18:00 -0400

    linkedlist

--- 0113/Insert In Sorted Linked List.java
+++ 0113/Insert In Sorted Linked List.java
@@ -11,18 +11,14 @@
 public class Solution {
   public ListNode insert(ListNode head, int value) {
     ListNode node = new ListNode(value);
-    if (head == null || head.value >= value) {
+    if (head == null) return node;
+    if (head.value >= value) {
       node.next = head;
       return node;
-    }
+    } 
     ListNode cur = head;
-    // find the last element that should be in the left side of the new node
-    while (cur != null && cur.next != null) {
-      if (cur.value <= value && cur.next.value <= value) {
-        cur = cur.next;
-      } else {
-        break;
-      }
+    while (cur.next != null && cur.next.value < value) {
+      cur = cur.next;
     }
     node.next = cur.next;
     cur.next = node;
